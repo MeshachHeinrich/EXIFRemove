ff30a89e3f0c35e6d8db530d86f245ae95dbe90c
MeshachHeinrich <42801000+MeshachHeinrich@users.noreply.github.com>
2024-03-07 02:18:43 +1030

    Now creates a new EXIF-stripped file
    Also added coloured text to help with visual clarity of console output

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import sys
 import PIL.Image
 from PIL.ExifTags import TAGS
 
+# Define colour printing functions
+def printYellow(text): print("\033[93m{}\033[00m".format(text), end='')
+def printBlue(text): print("\033[96m{}\033[00m".format(text), end='')
+
 # Run the program
-print("Running EXIFRemove...\n")
+printYellow("""\
+    
+    _______  __ ______________                               
+   / ____/ |/ //  _/ ____/ __ \___  ____ ___  ____ _   _____ 
+  / __/  |   / / // /_  / /_/ / _ \/ __ `__ \/ __ \ | / / _ \\
+ / /___ /   |_/ // __/ / _, _/  __/ / / / / / /_/ / |/ /  __/
+/_____//_/|_/___/_/   /_/ |_|\___/_/ /_/ /_/\____/|___/\___/ 
+\n""")
+
+printBlue("Brought to you by Meshach Heinrich\n\n")
+
 
 # Read input arguments
 if (len(sys.argv) == 1):
@@ -23,9 +37,15 @@ else:
     
     for key, val in exif.items():
         if key in TAGS:
-            print(f'{TAGS[key]} : {val}')
+            printYellow(f'{TAGS[key]}')
+            print(f': {val}')
         else:
-            print(f'{key} : {val}') 
+            printYellow(f'{key} : {val}') 
     
     print("----------------------------------------------------------------\n")
 
+# Remove EXIF data
+output = PIL.Image.new(image.mode, image.size)
+output.putdata(list(image.getdata()))
+output.save("Output.JPEG")
+output.close()
