ab30cfdafc07e2970b1d33404b915a869eb151a8
MeshachHeinrich <42801000+MeshachHeinrich@users.noreply.github.com>
2024-03-06 16:24:07 +1030

    Created basic main function
    Simply checks if input arguments were provided

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+print("Running EXIFRemove...")
+
+# Read input arguments
+if (len(sys.argv) == 1):
+     print("No file inputs provided")
+     exit()
+     
+print(sys.argv[1])
+    
+    
