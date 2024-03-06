5be71fbd5b76944296a67ee937908b54ac71cb56
MeshachHeinrich <42801000+MeshachHeinrich@users.noreply.github.com>
2024-03-06 17:11:14 +1030

    EXIF Data is now printed to the console

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import sys
+import PIL.Image
+from PIL.ExifTags import TAGS
 
-print("Running EXIFRemove...")
+# Run the program
+print("Running EXIFRemove...\n")
 
 # Read input arguments
 if (len(sys.argv) == 1):
      print("No file inputs provided")
      exit()
      
-print(sys.argv[1])
+# Retrieve EXIF data from image
+image = PIL.Image.open(sys.argv[1])
+exif = image._getexif()
+
+# Display EXIF data
+if exif is None:
+    print("File has no EXIF data")
+    exit()
+else:
+    print("EXIF data: ------------------------------------------------------\n")
     
+    for key, val in exif.items():
+        if key in TAGS:
+            print(f'{TAGS[key]} : {val}')
+        else:
+            print(f'{key} : {val}') 
     
+    print("----------------------------------------------------------------\n")
+
